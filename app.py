2d9fa701d69f8faf74daace542ae1ceffede4ca3
theafreen <Afreenshaik216@gmail.com>
2025-06-28 13:16:29 +0530

    commit

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 print("hi")
 print("hello world")
-pritn("afreen")
+print("afreen")
--- train.py
+++ train.py
@@ -1,2 +1,11 @@
 print("afreen")
 print("hello world")
+print("git checkoout feature1")
+print("this is written on branch feature1")
+
+print("this is to show merge conflict")
+
+print("git checkoout feature1")
+print("this is written on branch feature1")
+
+print("this is local change ")
